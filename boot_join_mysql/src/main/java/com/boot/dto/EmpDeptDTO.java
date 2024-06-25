6dac7f66412a728e82b6430925ce00c43ff72483
cucudass <whrhkdfo12@naver.com>
2024-06-25 17:45:32 +0900

    spring_boot example

---
+++ boot_join_mysql/src/main/java/com/boot/dto/EmpDeptDTO.java
@@ -0,0 +1,27 @@
+package com.boot.dto;
+
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmpDeptDTO {
+	//mxl 정보뿐만 아니라 전체 테이블의 컬럼을 정의(개발시 유연성: DTO 공유)
+	//emp
+	private int empno;
+	private String ename;
+	private String job;
+	private int mgr;
+	private Timestamp hiredate;
+	private int sal;
+	private int comm;
+	
+	//dept
+	private int deptno;
+	private String dname;
+	private String loc;
+}
